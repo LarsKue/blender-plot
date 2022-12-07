9768d1e9f66d5103de460d8f69c4da04cb0031c4
LarsKue <lars@kuehmichel.de>
2022-12-07 17:02:52 +0100

    pypi version bump, added install test

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -10,6 +10,20 @@ def test_blender_version():
     assert bpy.app.version == (3, 4, 0)
 
 
+def test_install():
+    import sys
+    import os
+
+    os.system(" ".join([sys.executable, "-m", "venv", ".test_venv"]))
+    os.system(" ".join(["source", ".test_venv/bin/activate"]))
+    os.system(" ".join(["pip", "install", "-U", "pip", "setuptools", "wheel"]))
+    failed = os.system(" ".join(["pip", "install", "."]))
+    os.system(" ".join(["deactivate"]))
+    os.system(" ".join(["rm", "-rf", ".test_venv"]))
+
+    assert not failed
+
+
 def test_pip_install():
     import pip
     failed = pip.main(["install", "blender-plot"])
